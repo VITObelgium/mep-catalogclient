ab196e7c0b2e4684455106f7268a93b761ba779c
Dirk Daems <dirk.daems@gmail.com>
2017-01-09 13:33:48 +0100

    Set dev version to 0.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 test_requirements = ['requests','mock']
 
 setup(name='catalogclient',
-      version='0.3',
+      version='0.4',
       author='Dirk Daems',
       author_email='dirk.daems@vito.be',
       description='Catalog client for the internal PROBA-V MEP and Copernicus Global Land catalogs',
